0043dc1cb68acd8d5ce00facd0dd16d3938a81f1
SureshKumarJeeva <jsureshkumarj1@gmail.com>
2025-12-16 19:19:47 +0530

    removed react serve statements for Render

--- ems-server/src/server.js
+++ ems-server/src/server.js
@@ -37,16 +37,16 @@ export async function startServer() {
     })
   );
 
-  // Path to React build folder
-  const reactBuildPath = path.join(__dirname, "../../ems-client/build");
+  // // Path to React build folder
+  // const reactBuildPath = path.join(__dirname, "../../ems-client/build");
 
-  // Serve React frontend build
-  app.use(express.static(reactBuildPath));
+  // // Serve React frontend build
+  // app.use(express.static(reactBuildPath));
 
-  // Fallback to index.html for React routing
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(reactBuildPath,"index.html"));
-  });
+  // // Fallback to index.html for React routing
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.join(reactBuildPath,"index.html"));
+  // });
 
   const PORT = process.env.PORT || 4000;
   app.listen(PORT, () => {
