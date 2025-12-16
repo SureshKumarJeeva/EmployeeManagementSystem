8b89110c12f86e2c34422a1c94c19f38c33b2d7b
SureshKumarJeeva <jsureshkumarj1@gmail.com>
2025-12-16 19:32:57 +0530

    Backedn server url updated with Render deployed url

--- ems-client/src/apolloClient.js
+++ ems-client/src/apolloClient.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = new HttpLink ({
-  uri: "http://localhost:4000/graphql", // IMPORTANT: same server, proxy through Express
+  uri: "https://ems-backend-x7gp.onrender.com/graphql", // IMPORTANT: same server, proxy through Express
 });
 
 const authLink = setContext((_, { headers }) => {
